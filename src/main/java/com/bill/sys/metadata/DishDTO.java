0736f680195f42c3bbdf1dea4456b55a49385960
zhujiancom <eric87com@gmail.com>
2015-04-09 23:20:17 +0800

    添加baseEntity

---
+++ src/main/java/com/bill/sys/metadata/DishDTO.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.bill.sys.metadata;
+
+/**
+ * @author zj
+ *	
+ * 项目名称：BillSystem
+ *
+ * 类名称：DishDTO
+ *
+ * 包名称：com.bill.sys.metadata
+ *
+ * Operate Time: 2015年4月9日 下午11:16:24
+ *
+ * remark (备注):
+ *
+ * 文件名称：DishDTO.java
+ *
+ */
+public class DishDTO {
+
+}
